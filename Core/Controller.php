f0377281eed5c084329644f1cb387225dd4a2e51
emilie <emilie.le-lan@epitech.eu>
2024-04-16 16:12:36 +0200

    add render method

--- Core/Controller.php
+++ Core/Controller.php
@@ -4,11 +4,33 @@ namespace Controller;
 
 use Model\UserModel;
 
-class UserController {
+class Controller {
 
-    public function  getUserById($id) {
-        $userModel = new UserModel();
-        $user = $userModel->getById($id);
+   protected static $_render;
+
+   protected function render($view, $scope = []) {
+    extract($scope);
+    $viewFilePath = implode(DIRECTORY_SEPARATOR, [
+        dirname(__DIR__), 
+        'src', 
+        'View', 
+        str_replace('Controller', '', basename(get_class($this))), 
+        $view
+    ]) . '.php';
+    
+    if (file_exists($viewFilePath)) {
+        ob_start();
+        include $viewFilePath;
+        $viewContent = ob_get_clean();
+        
+        ob_start();
+        include implode(DIRECTORY_SEPARATOR, [
+            dirname(__DIR__), 
+            'src', 'View', 'index']) . '.php';
+
+        self::$_render = ob_get_clean();
+        
+        }       
     }
 }
 
