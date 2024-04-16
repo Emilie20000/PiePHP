d9720198b4ac3807fb31ab10d18e20ed48dc87f4
emilie <emilie.le-lan@epitech.eu>
2024-04-16 11:38:52 +0200

    change run method for run the router

--- Core/Core.php
+++ Core/Core.php
@@ -7,10 +7,20 @@
 namespace Core;
 
 class Core {
+
+    protected $router;
+
+    public function __construct($router) {
+        $this->router = $router;
+    }
     
     public function run() {
 
         echo __CLASS__ . " [OK] " . PHP_EOL;
+
+        $uri = $_SERVER['REQUEST_URI'];
+
+        $this->router->matchUri($uri);
     }
 }
 
