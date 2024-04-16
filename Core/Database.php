1dffec9bc8b0ce9a3e3a5ea74c134d55b29bf6aa
emilie <emilie.le-lan@epitech.eu>
2024-04-16 11:35:02 +0200

    add database connection

--- Core/Database.php
+++ Core/Database.php
@@ -2,6 +2,9 @@
 
 namespace Core;
 
+use PDO;
+use Exception;
+
 class Database {
 
     private static $connection;
@@ -10,27 +13,28 @@ class Database {
 
         if (!self::$connection) {
 
-            $host = getenv('DB_HOST');
-            $username = getenv('DB_USERNAME');
-            $password = getenv('DB_PASSWORD');
-            $database = getenv('DB_DATABASE');
-
-            $dsn = 'mysql:host=' . $host . 'dbname=' . $database;
+            define('DB_HOST', '');
+            define('DB_NAME', '');
+            define('DB_USER', '');
+            define('DB_PASSWORD', '');
 
             try {
-                self::$connection = new PDO($dsn, $username, $password);
-                self::$connection->setAttribute(
-                    PDO::ATTR_ERRMODE, 
-                    PDO::ERRMODE_EXCEPTION
-                );
-
+                self::$connection = new PDO('mysql:host=' . DB_HOST . 
+                                            ';dbname=' . DB_NAME,
+                                                         DB_USER,
+                                                         DB_PASSWORD);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, 
+                                                PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
-                throw new Exception(
-                    'Erreur de connexion à la base de données : ' . 
-                    $e->getMessage());
+                throw new Exception('Erreur de connexion à la bdd : '
+                                     . $e->getMessage());
             }
         }
+
+        return self::$connection;
     }
 }
+
+
 
 ?>
