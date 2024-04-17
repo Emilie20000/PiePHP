8f596538cc3bddf0ef0da9013f885e1c95eee45f
emilie <emilie.le-lan@epitech.eu>
2024-04-17 14:58:47 +0200

    use request method for secure HTTP inputs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,35 @@
 
 namespace Controller;
 
+use Core\Controller;
 use Model\UserModel;
 
-class UserController {
-    public function index() {
-        $userModel = new UserModel();
-        $users = $userModel->getusers();
-        print_r($users);
+class UserController extends Controller {
+
+    public function addAction() {
+        $this->render('register');
     }
+
+    public function registerAction() {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $email = $this->request->post('email');
+            $password = $this->request->post('password');
+
+            $userModel = new UserModel($email, $password);
+
+            $register = $userModel->create();
+
+            if ($register) {
+                echo 'Inscription réussie';
+            } else {
+                echo 'Erreur lors de l\'inscription';
+            }
+
+        }
+    }
+    
 }
 
 ?>
