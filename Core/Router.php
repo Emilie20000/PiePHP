8c9cde200bb11c76b51d6df819c81187a3afa733
emilie <emilie.le-lan@epitech.eu>
2024-04-17 11:02:15 +0200

    add dinamic router

--- Core/Router.php
+++ Core/Router.php
@@ -10,15 +10,31 @@ class Router {
         self::$routes[$url] = $route;
     }
 
-    public static function get($url) {
+    /*public static function get($url) {
 
         if (array_key_exists($url, self::$routes)) {
             $route = self::$routes[$url];
 
             return $route;
         }
+    }*/
+
+    public static function get($url) {
+        $urlParts = explode('/', $url);
+
+        $piePHPIndex = array_search('PiePHP', $urlParts);
+
+        $controllerName = !empty($urlParts[$piePHPIndex + 1])  
+            ? ($urlParts[$piePHPIndex + 1])
+            : 'app';
+
+        $actionName = !empty($urlParts[$piePHPIndex + 2])
+        ? ($urlParts[$piePHPIndex + 2])
+        : 'index';
+    
+        return ['controller' => $controllerName, 'action' => $actionName];
     }
-}
+}   
 
 ?>
 
