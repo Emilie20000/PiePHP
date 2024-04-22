17789935fe00c5efddbb47c8a5b4f710079e8384
emilie <emilie.le-lan@epitech.eu>
2024-04-22 10:49:11 +0200

    add constructor for get model attrributsby an array or an id

--- Core/Entity.php
+++ Core/Entity.php
@@ -1,9 +1,18 @@
 <?php
 
+namespace Core;
+
 class Entity {
 
-    public function __construct() {
-        
+    public function __construct(array $data) {
+
+        if (count($data) === 1 && isset($data['id'])) {
+            $this->id = $data['id'];
+        } else {
+            foreach ($data as $key => $value) {
+                $this->{$key} = $value;
+            }
+        }
     }
 
 }
