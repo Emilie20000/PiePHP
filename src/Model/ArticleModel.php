cee3744a5fb91ae0e9e9ed8bea013aa1cdee9cb0
emilie <emilie.le-lan@epitech.eu>
2024-04-23 10:57:45 +0200

    add construct method

--- src/Model/ArticleModel.php
+++ src/Model/ArticleModel.php
@@ -4,11 +4,20 @@ namespace Model;
 
 class ArticleModel extends Entity {
 
-    private static $relations = [
-        'has_many' => 'comments'
+    private $orm;
+    private static $relation = [
+        'has_many' => 'tags'
     ];
 
-   
+    public function __construct($data) {
+        parent::__construct($data);
+        $this->orm = new ORM;
+        
+    }
+
+   public function getTags() {
+
+   }
      
 
     
