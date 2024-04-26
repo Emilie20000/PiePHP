361fc3afda95d36b19d7e00c9fb0909adb296746
emilie <emilie.le-lan@epitech.eu>
2024-04-26 10:02:19 +0200

    add new inputs in form

--- src/View/User/register.php
+++ src/View/User/register.php
@@ -10,7 +10,19 @@
                 <form action="user/register" method="post" name="register_form" id="registerForm" class="">
                     <div class="">
                         <h2 class="">Inscription</h2>
+                    </div>
+                    <div class="">
+                        <label for="">Prénom :</label>
+                        <input class="" type="text" name="firstname" id="firstname" required>
                     </div> 
+                    <div class="">
+                        <label for="">Nom :</label>
+                        <input class="" type="text" name="lastname" id="lastname" required>
+                    </div>
+                    <div class="">
+                        <label for="">Date de naissance :</label>
+                        <input class="" type="date" name="birthdate" id="birthdate" required>
+                    </div>  
                     <div class="">
                         <label for="email">Email :</label>
                         <input class="" type="email" name="email" id="email" required>
@@ -18,6 +30,10 @@
                     <div class="">
                         <label for="password">Mot de passe :</label>
                         <input class="" type="password" name="password" id="password" required>
+                    </div>
+                    <div class="">
+                        <label for="password">Confirmation du mot passe :</label>
+                        <input class="" type="password" name="confirmPassword" id="confirmPassword" required>
                     </div>
                     <div class="">
                         <button class="" id="register">S'inscrire</button>
