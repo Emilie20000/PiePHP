ecc21a03af675bdc37171d63c58a3a457da01ef1
emilie <emilie.le-lan@epitech.eu>
2024-04-27 13:32:08 +0200

    add register functionnalities

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,11 @@
 namespace Controller;
 
 use Core\Controller;
+use Core\SecurityUtils;
 use Model\UserModel;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 class UserController extends Controller {
 
@@ -11,37 +15,45 @@ class UserController extends Controller {
 
     public function __construct() {
         parent::__construct();
+        
     }
 
-    public function addRegisterAction() {
-        $this->render('register');
-    }
-
-    public function addLoginAction() {
-        $this->render('login');
+    public function indexAction() {
+        $this->render('index');
     }
 
     public function registerAction() {
 
+        $this->render('register');
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-            $email = $this->request->post('email');
-            $password = $this->request->post('password');
-
-            $data = [
-                'email' => $email,
-                'password' => $password
-            ];
-
-            $userModel = new UserModel($data);
-
-            $register = $userModel->createUser($data);
+            $params = $this->request->getQueryParams();
 
 
-            if ($register) {
+            if (!SecurityUtils::passwordMatch($params['password'], 
+                                              $params['confirm_password'])) {
+                echo 'Les mots de passe ne correspondent pas';
+                return false;
+            }
+
+            if (!SecurityUtils::securePassword($params['password'])) {
+                echo 'Le mot de passe doit mesurer au moins 8 caratères et contenir : 
+                        - des lettre majuscules et minuscule
+                        - des chiffres
+                        - des caractères spéciaux';
+                return false;
+            }
+
+            unset($params['confirm_password']);
+            $userModel = new UserModel($params);
+            $user = $userModel->findByEmail();
+
+            if (!$userModel->id) {
+                $userModel->createUser();
                 echo 'Inscription réussie';
             } else {
-                echo 'Erreur lors de l\'inscription';
+                echo 'Cet email est déjà associé à un compte';
             }
 
         }
--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -4,26 +4,65 @@ namespace Model;
 
 use Core\Entity;
 use Core\ORM;
+use Core\SecurityUtils;
 
 class UserModel extends Entity {
 
-    private $orm;
-    private $table = 'users';
+    private $table = 'user';
+    private $id;
+    protected $firstname;
+    protected $lastname;
+    protected $birthdate;
     protected $email;
     protected $password;
 
     public function __construct($data) {
         parent::__construct($data);
-        $this->orm = new ORM;
-        $this->email = $data['email'];
-        $this->password = $data['password'];
+        
+    }
+
+    public function __get($attribut) {
+        if ($attribut === 'id') {
+            return $this->getId();
+        }
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function findByEmail() {
+        $params = [
+            'WHERE' => 'email = :email'
+        ];
+
+        $email = $this->email;
+        $params['params'] = [':email' => $email];
+        $user = $this->orm->find($this->table, $params);
+
+        if ($user) {
+            $this->setId($user['id']);
+        }
+    
+        return $user;
+
     }
 
     public function createUser() {
+        $this->password = SecurityUtils::hashPassword($this->password);
         $fields = [
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'birthdate' => $this->birthdate,
             'email' => $this->email,
             'password' => $this->password
         ];
+
+        var_dump($fields);
 
         return $this->orm->create($this->table, $fields);
     }
