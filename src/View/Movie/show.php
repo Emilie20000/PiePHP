5f184e02c03d3ac76babf99de4c7936de996f32a
emilie <emilie.le-lan@epitech.eu>
2024-04-28 16:45:46 +0200

    display distributor name on movie details

--- src/View/Movie/show.php
+++ src/View/Movie/show.php
@@ -7,14 +7,18 @@
 </head>
 <body>
 
-    <?php foreach ($movie as $film) : ?>
+    <?php foreach ($movie['main'] as $film) : ?>
         <h1><?php echo $film['title']; ?></h1>
         <p><strong>Réalisateur:</strong> <?php echo $film['director']; ?></p>
         <p><strong>Durée:</strong> <?php echo $film['duration']; ?> minutes</p>
         <p><strong>Date de sortie:</strong> <?php echo $film['release_date']; ?></p>
-        <p><strong>Classification:</strong> <?php echo $film['rating']; ?></p>
-        <p><strong>Distributeur:</strong> <?php echo $film['name']; ?></p>
-
+        <p><strong>Age minimum:</strong> <?php echo $film['rating']; ?></p>
     <?php endforeach; ?>
+
+    <?php foreach ($movie['relation'] as $distributor) : ?>
+        <p><strong>Distributeur:</strong> <a href="<?= BASE_URI ?>/distributor/show/<?= $distributor['id'] ?>">
+        <?php echo $distributor['name']; ?></a></p>
+    <?php endforeach; ?>
+
 </body>
 </html>
