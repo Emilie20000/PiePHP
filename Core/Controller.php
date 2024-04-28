7a626c871db85fefbe29f56e8c62632b97576543
emilie <emilie.le-lan@epitech.eu>
2024-04-28 13:16:53 +0200

    add paginator method

--- Core/Controller.php
+++ Core/Controller.php
@@ -3,6 +3,7 @@
 namespace Core;
 
 use Core\Request;
+use Core\TemplateEngine;
 
 class Controller {
 
@@ -11,11 +12,12 @@ class Controller {
 
    public function __construct() {
     $this->request = new Request();
+    $this->templateEngine = new TemplateEngine();
    }
 
    public function __destruct() {
     echo self::$_render;
-}
+    }
 
    protected function render($view, $scope = []){
         extract($scope);
@@ -35,13 +37,36 @@ class Controller {
             include(implode(DIRECTORY_SEPARATOR, 
             [dirname(__DIR__), 'src', 'View', 'index'])
              . '.php');
-
             self::$_render = ob_get_clean();
             
         }
     }
-}
 
+    protected function createSession($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    protected function redirect($url) {
+        header('Location: ' . BASE_URI . $url);
+    }
+
+    protected function paginator($currentPage, $perPage, $totalItems) {
+        
+        $totalPages = ceil($totalItems / $perPage);
+    
+        $pagination = [
+            'current_page' => $currentPage,
+            'total_pages' => $totalPages,
+            'next_page' => ($currentPage < $totalPages) 
+                                ? $currentPage + 1 : null,
+            'prev_page' => ($currentPage > 1) 
+                                ? $currentPage - 1 : null,
+        ];
+    
+        return $pagination;
+    }
+
+}
 
 
 
