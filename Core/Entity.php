0477d66982583fb061064d296ca722ab216759f8
emilie <emilie.le-lan@epitech.eu>
2024-04-28 18:27:17 +0200

    fix Entity setting functionnalities

--- Core/Entity.php
+++ Core/Entity.php
@@ -4,13 +4,29 @@ namespace Core;
 
 class Entity {
 
+    protected $orm;
+
     public function __construct(array $data) {
 
-        if (count($data) === 1 && isset($data['id'])) {
-            $this->id = $data['id'];
+        $this->orm = new ORM();
+
+        if (isset($data['id']) && count($data) === 1) {
+            $this->setDataById($data['id']);
         } else {
-            foreach ($data as $key => $value) {
-                $this->{$key} = $value;
+            $this->setData($data);
+        }
+    }
+
+    protected function setDataById($id) {
+        $data = $this->orm->read($this->table, static::class, $id);
+        $this->setData($data);
+    }
+
+    protected function setData(array $data) {
+        foreach ($data as $key => $value) {
+            $setterMethod = 'set' . ucfirst($key);
+            if (method_exists($this, $setterMethod)) {
+                $this->{$setterMethod}($value);
             }
         }
     }
